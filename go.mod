0f5e05f81404f5e7bbd4491eb3938ba180d18ca7
advanced-go <mark.wilson.go@outlook.com>
2024-06-10 08:37:34 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/advanced-go/backbone-test
+
+go 1.21.4
+
+require github.com/advanced-go/stdlib v0.0.0-20240609152119-8e3aa89b2647
+
+require github.com/google/uuid v1.6.0 // indirect
---
+++ pkg/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/advanced-go/stdlib/httpx"
+	io2 "github.com/advanced-go/stdlib/io"
+	"net/http"
+)
+
+const (
+	GuidanceUrl    = "http://localhost:8081/github/advanced-go/guidance:resiliency"
+	ObservationUrl = "http://localhost:8081/github/advanced-go/observation:timeseries"
+
+	GuidanceResource    = "file://[cwd]/pkg/resource/documents-v1.json"
+	ObservationResource = "file://[cwd]/pkg/resource/access-v1.json"
+)
+
+func main() {
+	//testSearch()
+	initialLoad()
+	//testAgentLoad()
+
+}
+
+func initialLoad() {
+	Put(GuidanceResource, GuidanceUrl, "")
+	Put(ObservationResource, ObservationUrl, "")
+}
+
+func Put(file, uri, variant string) bool {
+	buf, status := io2.ReadFile(file)
+	if !status.OK() {
+		fmt.Printf("read file err: %v\n", status.Err)
+		return false
+	}
+	reader := bytes.NewReader(buf)
+	req, err1 := http.NewRequest(http.MethodPut, uri, reader)
+	if err1 != nil {
+		fmt.Printf("new request err: %v\n", err1)
+		return false
+	}
+	resp, status1 := httpx.Do(req)
+	if resp != nil {
+		fmt.Printf("StatusCode: %v\n", resp.StatusCode)
+	}
+	fmt.Printf("Put() [status:%v]\n", status1)
+	return true
+}
